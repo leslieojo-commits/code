d09b7db85d8961a474dbd1cdc9e7742e999a4e66
Leslie Ojo <leslie.ojo@student.hhs.school.nz>
2026-05-22 10:59:12 +1200

    Setting up Program structure through comments

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hello world!")
+# import 
+ #-pygame
+ #-imagelist
+ #-debug
+
+#constants
+SCR_X = 640
+SCR_Y = 480
+WINDOW_MODE = pygame.RESIZABLE
+
+#Class defintions
+ #-button class etc.
+
+#function defs
+
+if __name__="main":
+    #INITIALIZATION
+        #set up pygame
+        pygame.init()
+        screen = pygame.display.set-mode((SCR_X, SCR_Y), WINDOW_MODE)
+        #set up variables
+
+    #Main loop of the game running - until quit
+
+        #check events - has anything happened (has anyone clicked a button, exited window)
+
+            #do something, or set a variable
+
+        #take action on events (Sounds etc)
+        
+        # do stuff that happens everytime 
+            #-animations
+            #check for things happening
+
+        # Clear the Screen
+        # Draw everything

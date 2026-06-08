861eecd0adffc6c6694accc5f0535b96789efff1
Leslie Ojo <leslie.ojo@student.hhs.school.nz>
2026-06-08 12:20:13 +1200

    First draft version of my snake game

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
-# import 
- #-pygame
- #-imagelist
- #-debug
+''' FIRST DRAFT VERSION OF MY SNAKE GAME '''
 
-#constants
-SCR_X = 640
-SCR_Y = 480
-WINDOW_MODE = pygame.RESIZABLE
 
-#Class defintions
- #-button class etc.
+import pygame
+import time
+pygame.init
 
-#function defs
+#screen name & size
+pygame.display.set_caption("Not Snake game")
+screen = pygame.display.set_mode((1000,720), pygame.RESIZABLE) # will need to fix the magic numbers in next version
 
-if __name__="main":
-    #INITIALIZATION
-        #set up pygame
-        pygame.init()
-        screen = pygame.display.set-mode((SCR_X, SCR_Y), WINDOW_MODE)
-        #set up variables
+'''-----CONSTANTS-----'''
 
-    #Main loop of the game running - until quit
+#RGB Tuples
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-        #check events - has anything happened (has anyone clicked a button, exited window)
+#Snake Size 
+snake_x = 490
+snake_y = 250
 
-            #do something, or set a variable
+#Snake size/movement coordinates
+snake_x_change = 10
+snake_y_change = 10
 
-        #take action on events (Sounds etc)
-        
-        # do stuff that happens everytime 
-            #-animations
-            #check for things happening
+# Drawing snake rectangles
+pygame.draw.rect(screen, RED, [snake_x, snake_y, 20, 20]) 
 
-        # Clear the Screen
-        # Draw everything
+quit_game = False
+
+while not quit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_game = True
+        elif event.type == pygame.K_LEFT:
+            snake_x_change = -20
+            snake_y_change = 0
+        elif event.type == pygame.K_RIGHT:
+            snake_x_change = 20
+            snake_y_change = 0
+        elif event.type == pygame.K_UP:
+            snake_x_change = 0
+            snake_y_change = -20
+        elif event.type == pygame.K_DOWN:
+            snake_x_change = 0
+            snake_y_change = 20
+
+    #increments
+    snake_x += snake_x_change
+    snake_y += snake_y_change
+
+pygame.display.update()
+
+# time-based snake movement
+clock = pygame.time.Clock()
+
+#Background
+screen.fill (GREEN)
+
+pygame.quit()
+quit()

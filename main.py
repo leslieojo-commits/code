c1a52ee358732c17fe525de0f9e71d6f70f54848
Leslie Ojo <leslie.ojo@student.hhs.school.nz>
2026-06-09 13:58:44 +1200

    Added draft version of Game Over Logic & Randomizer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import pygame
 import time
+import random
 pygame.init()
 
 #screen name & size
@@ -17,10 +18,19 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+WHITE = (255,255, 255)
 
 # Snake Size 
 snake_x = 490
 snake_y = 250
+
+#Food Size
+food_x = 490
+food_y = 250
+
+#Centre Size
+center_x = food_x 
+center_y = food_x 
 
 # time-based snake movement
 clock = pygame.time.Clock()
@@ -28,6 +38,18 @@ clock = pygame.time.Clock()
 #Snake size/movement coordinates
 snake_x_change = 0
 snake_y_change = 0
+
+# Game Over MSG
+font = pygame.font.Font("freesansbold.ttf", 50)
+
+# Food randomizer
+food_x = round(random.randrange(20,1000-20)/20)*20
+
+#Function for displaying Game Over message
+def message (msg, txt_colour):
+    txt = font.render(msg, True, txt_colour)
+    text_box = txt.get_rect(center=(500, 360))
+    screen.blit(txt, text_box)
 
 quit_game = False
 
@@ -53,13 +75,26 @@ while not quit_game:
     snake_x += snake_x_change
     snake_y += snake_y_change
 
-    #Background
+            # Code to quit fame, once bounds is reached
+    if snake_x >= 1000 or snake_x < 0 or snake_y >= 720 or snake_y < 0:
+        quit_game = True
+
+
+    # Drawing Background
     screen.fill (GREEN)
-    pygame.draw.rect(screen, RED, [snake_x, snake_y, 20, 20]) 
+    pygame.draw.circle  (screen, RED, (snake_x, snake_y), 10)               #Temporarily making this more circular, because it looks sort of nicer
+    #pygame.draw.rect(screen, RED, [snake_x, snake_y, 20, 20]) 
+
+    # Drawing food
+    pygame.draw.circle  (screen, BLACK, (food_x, food_y), 10)
 
     # Drawing snake rectangles
     pygame.display.update()
 
-    clock.tick (1)
+    clock.tick (10)
+
+message ("Game over!", BLACK)
+pygame.display.update()
+time.sleep(1)
 
 pygame.quit()

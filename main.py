761a08b7d9334cd6360a7b485c249098435a2858
Leslie Ojo <leslie.ojo.hhs.school.nz>
2026-07-03 11:16:15 +1200

    Created a button Class & Object

--- main.py
+++ main.py
@@ -1,96 +1,95 @@
-''' FIRST DRAFT VERSION OF MY SNAKE GAME '''
-# test update from personal machine
-
+# import libraries
 import pygame
 import time
 import random
+
+# ==============
+# CONSTANTS
+# ==============
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors
+BLACK = pygame.Color(0, 0, 0)
+UI_WHITE = pygame.Color(255, 255, 255)
+BTN_RED = pygame.Color (173, 19, 28)
+BKGD_RED = pygame.Color (255, 117, 111)
+SNAKE_GREEN = pygame.Color (88, 122, 51)
+TILE_WHITE = pygame.Color (232, 232, 232)
+
+# Fonts
+FONT_FOLDER = "fonts"
+MAIN_FONT = f"{FONT_FOLDER}\\Poppins-Medium.ttf"
+MAIN_FONT_SIZE = 30
+
+
+# ==============
+# CLASSES
+# ==============
+
+class Button ():    # Button Class
+    #NORMAL_ANGLE = 0
+    #MAIN_MENU_ANGLE = 15 
+
+    #BUTTON_FONT_FOLDER = "fonts"
+    #BUTTON_FONT = f"{BUTTON_FONT_FOLDER}\\Poppins-Medium.ttf"
+    #FONT_SIZE = 20
+
+    def __init__(self, x, y, w, h, color, font, angle=0):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.font = font
+        self.angle = angle # rotatation angle
+
+        self.surface = pygame.Surface((w, h), pygame.SRCALPHA) # creates the button surface
+        self.rect = self.surface.get_rect(topleft = (-5, 0)) # creates the rectangle
+
+    def button_draw (self, game_window, text):  # draws text unto button and rotates button
+        self.surface.fill(self.color)
+        button_text = self.font.render(text, True, UI_WHITE)
+        button_text_rect = button_text.get_rect(center = (self.w // 2, self.h // 2))
+        self.surface.blit(button_text, button_text_rect)
+
+        rotated_surface = pygame.transform.rotate(self.surface, self.angle)
+        rotated_rect = rotated_surface.get_rect(center = (self.rect.center))
+        game_window.blit(rotated_surface, rotated_rect)
+
+
+
+# ==============
+# INITIALIZATION
+# ==============
+
 pygame.init()
-
-#screen name & size
 pygame.display.set_caption("Not Snake game")
-screen = pygame.display.set_mode((1000,720), pygame.RESIZABLE) # will need to fix the magic numbers in next version
+game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE) # screen size
 
-'''-----CONSTANTS-----'''
+MAIN_FONT = pygame.Font(MAIN_FONT, MAIN_FONT_SIZE) 
+clock = pygame.time.Clock() #Sets frame rate
 
-#RGB Tuples
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-WHITE = (255,255, 255)
+#buttons
+play_button = Button( 100, 100, 300, 100, BTN_RED, MAIN_FONT, angle= -5)
 
-# Snake Size 
-snake_x = 490
-snake_y = 250
-
-#Food Size
-food_x = 490
-food_y = 250
-
-# time-based snake movement
-clock = pygame.time.Clock()
-
-#Snake size/movement coordinates
-snake_x_change = 0
-snake_y_change = 0
-
-# Game Over MSG
-font = pygame.font.Font("freesansbold.ttf", 50)
-
-# Food randomizer
-food_x = round(random.randrange(20,1000-20)/20)*20
-
-#Function for displaying Game Over message
-def message (msg, txt_colour):
-    txt = font.render(msg, True, txt_colour)
-    text_box = txt.get_rect(center=(500, 360))
-    screen.blit(txt, text_box)
+# --- Game Loop ---
 
 quit_game = False
-
 while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit_game = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                snake_x_change = -20
-                snake_y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                snake_x_change = 20
-                snake_y_change = 0
-            elif event.key == pygame.K_UP:
-                snake_x_change = 0
-                snake_y_change = -20
-            elif event.key == pygame.K_DOWN:
-                snake_x_change = 0
-                snake_y_change = 20
+        if event.type == pygame.VIDEORESIZE: # allows game window to resize
+            SCREEN_X, SCREEN_Y = event.w, event.h
+            game_window = pygame.display.set_mode((SCREEN_X, SCREEN_Y), pygame.RESIZABLE)
+        #blah_blah = main_font.render("Blah Blah", SNAKE_GREEN, UI_WHITE)
+        #blah_blah_rect = blah_blah.get_rect()
+        #blah_blah_rect.center = ( SCREEN_X // 2, SCREEN_Y //2 )
 
-    #increments
-    snake_x += snake_x_change
-    snake_y += snake_y_change
-
-            # Code to quit fame, once bounds is reached
-    if snake_x >= 1000 or snake_x < 0 or snake_y >= 720 or snake_y < 0:
-        quit_game = True
-
-
-    # Drawing Background
-    screen.fill (GREEN)
-    pygame.draw.circle  (screen, RED, (snake_x, snake_y), 10)               #Temporarily making this more circular, because it looks sort of nicer
-    #pygame.draw.rect(screen, RED, [snake_x, snake_y, 20, 20]) 
-
-    # Drawing food
-    pygame.draw.circle  (screen, BLACK, (food_x, food_y), 10)
-
-    # Drawing snake rectangles
-    pygame.display.update()
-
-    clock.tick (10)
-
-message ("Game over!", BLACK)
-pygame.display.update()
-time.sleep(1)
-
+        game_window.fill(BKGD_RED) # draws background colour in an hidden buffer
+        play_button.button_draw(game_window, "Play")
+        #game_window.blit(blah_blah, blah_blah_rect)
+        pygame.display.flip() # updates draw in the foreground 
 pygame.quit()

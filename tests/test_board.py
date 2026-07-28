6e2135e25d0eb16d455b06970e871e9ffe84492c
Leslie Ojo <leslie.ojo@student.hhs.school.nz>
2026-07-28 12:30:42 +1200

    I added a test_board class to do a unit test on board.get_tile_rect. Added some additional commets too

---
+++ tests/test_board.py
@@ -0,0 +1,26 @@
+import pygame
+from game import Board
+
+def test_board_get_tile_rect():
+
+    """ Tests that get_tile_rect returns expected rectangle. """
+
+    pygame.init()
+
+    # Creates a board instance with given screen dimension.
+    board = Board(800, 600)
+
+    tile = board.get_tile_rect (5,3)
+
+    #  Calculates expected position of the tile based on coordinates
+    #  of the row and column, and size of each cell.
+    expected_x = board.rect.left + 5 * board.cell_size
+    expected_y = board.rect.top + 3 * board.cell_size
+
+    # Could use later as the tricky debugging error I faced expected_y = board.rect.top + 3 * board.cell_size
+
+    #  Verifies that actual tile position coordinates matches expected coordinates.
+    assert tile.x == expected_x
+    assert tile.y == expected_y
+    assert tile.width == board.cell_size
+    assert tile.height == board.cell_size
--- tests/test_button.py
+++ tests/test_button.py
@@ -1,25 +1,36 @@
 import pygame
-from game import Button # Go to my main code and bring Button class to this test file
+from game import Button 
 
 def test_button_initialization():
 
+    """ Tests that button initializes with correct attributes. """
+
     pygame.init()
+
+    #  Creates a default font object for the button.
     font = pygame.font.SysFont(None, 30) 
+
+    #  Initializes button 
     button = Button(text = "Play", x=0, y=0, w=300, h=100, normal_color=(255, 0, 0), hover_color=(0, 255, 0), font=font)
 
+    #  Verifies button initializes its correct attributes.
     assert button.text == "Play"
     assert button.w == 300
     assert button.h == 100
     assert button.hovered is False
 
-def test_button_hover(): # test to check mouse is being hovered
+def test_button_hover(): 
+
+    """ Test to see that button detects mouse hovering, and responds appropriately. """
 
     pygame.init()
     font = pygame.font.SysFont(None, 30)
     button = Button(text = "Play", x=0, y=0, w=300, h=100, normal_color=(255, 0, 0), hover_color=(0, 255, 0), font=font)
 
+    #  Creates object containing mouse position.
     mouse_position = (200, 80)
 
+    # Passes mouse position to update method to check collision.
     button.update(mouse_position)
 
     assert button.hovered is True
